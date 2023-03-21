a4b8937c4ca7d5dcdd4de733ad2125661491859b
Buğrahan <Bugraakin2534@gmail.com>
2023-03-21 12:10:47 +0300

    .Net 6.0 ile JWT

---
+++ CmcApi/CmcApi/User.cs
@@ -0,0 +1,15 @@
+﻿namespace CmcApi
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public bool Check { get; set; }
+    }
+}
---
+++ CmcApi/CmcApi/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CmcApi
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public bool Check { get; set; }
+    }
+}
